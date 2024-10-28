e8dae145afcc9281bbb67e7ba6a39fd350a5d1eb
jijongkwon <help3451@naver.com>
2024-10-28 09:38:03 +0900

    init: 프로젝트 생성

---
+++ backend/api/src/test/java/com/scare/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scare.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
