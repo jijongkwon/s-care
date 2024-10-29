6c745ca1563971cc3b0c6ffa236c65107e10453d
simhani1 <simhani1@gmail.com>
2024-10-29 16:52:08 +0900

    feat: router 및 request 객체 추가

---
+++ backend/fast-api/routers/stress.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from services.calculate_service import calculate_stress_index
+from exceptions.custom_exceptions import CalculateFailedException
+from models.request_models import HeartRateRequest
+
+router = APIRouter()
+
+@router.post("/fast/hr")
+def get_stress_index(data: HeartRateRequest):
+    try:
+        stress_index = calculate_stress_index(data.hr_data)
+        if stress_index is None:
+            raise CalculateFailedException()
+        content = {"code": "2000", "message": "SUCCESS", "data": {"stressIndex": stress_index}}
+        return JSONResponse(content=content, status_code=200)
+    except CalculateFailedException as e:
+        error_content = {"code": "5000", "message": str(e), "data": None}
+        return JSONResponse(content=error_content, status_code=500)
