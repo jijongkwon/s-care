e074a4b379c202b54084ef8912ccc07ebe620e1c
KimYeonWook511 <kimyeonwook511@gmail.com>
2024-10-30 23:56:07 +0900

    feat: OAuth2 사용자 정보 처리 서비스 구현
    - 회원인 경우 정보 업데이트를 진행합니다.
    - 회원이 아닌 경우 회원가입을 진행합니다.

---
+++ backend/api/src/main/java/com/scare/api/core/config/security/service/CustomOAuth2UserService.java
@@ -0,0 +1,86 @@
+package com.scare.api.core.config.security.service;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.scare.api.core.config.security.service.dto.CustomOAuth2User;
+import com.scare.api.core.config.security.service.dto.GoogleResponse;
+import com.scare.api.core.config.security.service.dto.OAuth2Response;
+import com.scare.api.core.config.security.service.dto.UserInfo;
+import com.scare.api.member.domain.Member;
+import com.scare.api.member.domain.Provider;
+import com.scare.api.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+	private final MemberRepository memberRepository;
+
+	@Override
+	@Transactional
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		// 부모 클래스의 메서드를 사용하여 객체를 생성함
+		OAuth2User oAuth2User = super.loadUser(userRequest);
+
+		// 제공자에 맞는 객체 받아오기
+		OAuth2Response oAuth2Response = getOAuth2Response(oAuth2User, userRequest.getClientRegistration().getRegistrationId());
+
+		// 넘어온 회원정보가 이미 우리 DB 테이블에 존재하는지 확인
+		UserInfo userInfo = UserInfo.builder()
+			.email(oAuth2Response.getEmail())
+			.nickname(oAuth2Response.getNickname())
+			.profileUrl(oAuth2Response.getProfileImage())
+			.build();
+		Provider provider = oAuth2Response.getProvider();
+		Member member = memberRepository.findByEmailAndProvider(userInfo.getEmail(), provider).orElse(null);
+
+		if (member == null) {
+			log.info("회원 없음 (회원가입 진행) -> email: {}, provider: {}", userInfo.getEmail(), provider.name());
+
+			member = saveMember(userInfo, provider);
+
+		} else {
+			log.info("회원 있음 (회원정보 업데이트) -> email: {}, provider: {}", userInfo.getEmail(), provider.name());
+
+			// 멤버 정보 업데이트
+			member.updateNicknameAndProfileUrl(userInfo.getNickname(), userInfo.getProfileUrl());
+		}
+
+		return CustomOAuth2User.from(member);
+	}
+
+	private OAuth2Response getOAuth2Response(OAuth2User oAuth2User, String registrationId) {
+		// 제공자에 맞는 객체
+		OAuth2Response oAuth2Response = null;
+
+		// 제공자 확인
+		if ("google".equals(registrationId)) {
+			oAuth2Response = GoogleResponse.builder()
+				.attribute(oAuth2User.getAttributes())
+				.build();
+		}
+
+		return oAuth2Response;
+	}
+
+	private Member saveMember(UserInfo userInfo, Provider provider) {
+		Member member = Member.builder()
+			.email(userInfo.getEmail())
+			.profileUrl(userInfo.getProfileUrl())
+			.nickname(userInfo.getNickname())
+			.provider(provider)
+			.build();
+
+		return memberRepository.save(member);
+	}
+
+}
