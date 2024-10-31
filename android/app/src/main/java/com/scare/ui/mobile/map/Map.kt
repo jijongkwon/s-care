3a1113c2973a6871b5884cd294de20e22dc32b85
LEEJW1953 <jiwonlee1953@naver.com>
2024-10-31 17:58:06 +0900

    feat: 지도 화면에 헤더 추가

--- android/app/src/main/java/com/scare/ui/mobile/map/Map.kt
+++ android/app/src/main/java/com/scare/ui/mobile/map/Map.kt
@@ -2,15 +2,21 @@ package com.scare.ui.mobile.map
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.NaverMap
+import com.scare.ui.mobile.common.TheHeader
 
 @ExperimentalNaverMapApi
 @Composable
 fun Map(modifier: Modifier = Modifier) {
-    Box(modifier = modifier) {
-        NaverMap(modifier = Modifier.fillMaxSize()) {}
+    Scaffold(
+        topBar = { TheHeader { } }
+    ) {
+        Box(modifier = modifier) {
+            NaverMap(modifier = Modifier.fillMaxSize()) {}
+        }
     }
 }
