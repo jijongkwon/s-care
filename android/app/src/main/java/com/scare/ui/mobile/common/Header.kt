35d5c60ab5372275fbe87cf199865c3252feed74
LEEJW1953 <jiwonlee1953@naver.com>
2024-10-31 17:57:52 +0900

    design: 헤더 레이아웃 수정, 배경 설정

--- android/app/src/main/java/com/scare/ui/mobile/common/Header.kt
+++ android/app/src/main/java/com/scare/ui/mobile/common/Header.kt
@@ -1,49 +1,32 @@
 package com.scare.ui.mobile.common
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.work.Configuration
 import coil.compose.AsyncImage
 import com.scare.R
 
 @Composable
-fun TheHeader (
+fun TheHeader(
     imageUrl: String? = null,
     isMainPage: Boolean = false,
-    onBackClick: () -> Unit = {}
+    onBackClick: () -> Unit = {},
 ) {
-    Row (
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
+    Row(
+        modifier = Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.background).statusBarsPadding(),
 
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
+        horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically
     ) {
         if (isMainPage && imageUrl != null) {
             AsyncImage(
@@ -67,7 +50,7 @@ fun TheHeader (
 }
 
 @Composable
-fun Logo (modifier: Modifier = Modifier) {
+fun Logo(modifier: Modifier = Modifier) {
     val logoImage = if (isSystemInDarkTheme()) {
         painterResource(R.drawable.logo_white) // 다크 모드: 흰색 로고
     } else {
@@ -75,9 +58,7 @@ fun Logo (modifier: Modifier = Modifier) {
     }
 
     Image(
-        painter = logoImage,
-        contentDescription = "Logo",
-        modifier = Modifier.width(80.dp) // 가로 크기 200dp로 조정
-                            .height(60.dp) // 세로 크기 60dp로 고정 // 크기 조정
+        painter = logoImage, contentDescription = "Logo", modifier = Modifier.width(80.dp) // 가로 크기 200dp로 조정
+            .height(60.dp) // 세로 크기 60dp로 고정 // 크기 조정
     )
 }
