d48d78106a1e17bec8505e6f5b96c1e6d3d5070c
simhani1 <simhani1@gmail.com>
2024-10-31 15:54:06 +0900

    feat: 산책 코스 상세조회 service 구현

--- backend/api/src/main/java/com/scare/api/solution/walk/service/WalkingService.java
+++ backend/api/src/main/java/com/scare/api/solution/walk/service/WalkingService.java
@@ -2,7 +2,13 @@ package com.scare.api.solution.walk.service;
 
 import org.springframework.stereotype.Service;
 
+import com.scare.api.solution.walk.domain.WalkingCourse;
+import com.scare.api.solution.walk.domain.WalkingDetail;
+import com.scare.api.solution.walk.exception.NoWalkingCourseException;
+import com.scare.api.solution.walk.exception.NoWalkingDetailException;
 import com.scare.api.solution.walk.repository.WalkingCourseRepository;
+import com.scare.api.solution.walk.repository.WalkingDetailRepository;
+import com.scare.api.solution.walk.service.dto.WalkingCourseDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,4 +17,14 @@ import lombok.RequiredArgsConstructor;
 public class WalkingService {
 
 	private final WalkingCourseRepository walkingCourseRepository;
+	private final WalkingDetailRepository walkingDetailRepository;
+
+	public WalkingCourseDto getWalkingCourse(Long courseId) {
+		WalkingCourse walkingCourse = walkingCourseRepository.findById(courseId)
+			.orElseThrow(() -> new NoWalkingCourseException());
+		WalkingDetail walkingDetail = walkingDetailRepository.findById(walkingCourse.getId())
+			.orElseThrow(() -> new NoWalkingDetailException());
+		return WalkingCourseDto.from(walkingCourse, walkingDetail);
+	}
+
 }
