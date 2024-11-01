6159d56a6eb9d324436d247ada9e93e0039eac66
simhani1 <simhani1@gmail.com>
2024-11-01 13:34:52 +0900

    feat: 스트레스 정보 요청 router 추가

--- backend/fast-api/routers/stress.py
+++ backend/fast-api/routers/stress.py
@@ -1,31 +1,42 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from services.calculate_service import get_single_stress_index, get_multiple_stress_index
+
 from exceptions.custom_exceptions import CalculateFailedException
-from models.request_models import HeartRateRequest
+from models.request_models import SingleStressRequest, StressOverviewRequest
+from models.response_models import StressOverview, SuccessResponse, ErrorResponse, StressData
+from services.calculate_service import get_single_stress, get_multiple_stress_index
+from services.stress_overview_service import find_healing_course_idx
 
 router = APIRouter()
 
-@router.post("/fast/stress/calc/single")
-def calc_single_stress_index(data: HeartRateRequest):
-    try:
-        stress_index = get_single_stress_index(data.hr_data)
-        if stress_index is None:
-            raise CalculateFailedException()
-        content = {"code": "2000", "message": "SUCCESS", "data": {"stressIndex": stress_index}}
-        return JSONResponse(content=content, status_code=200)
-    except CalculateFailedException as e:
-        error_content = {"code": "5000", "message": str(e), "data": None}
-        return JSONResponse(content=error_content, status_code=500)
 
-@router.post("/fast/stress/calc/multiple")
-def calc_multiple_stress_index(data: HeartRateRequest):
+@router.post("/fast/stress/single")
+def calc_single_stress_index(data: SingleStressRequest):
     try:
-        stress_index_arr = get_multiple_stress_index(data.hr_data)
-        if stress_index_arr is None:
+        stress_idx = get_single_stress(data.hr_data)
+        if stress_idx is None:
             raise CalculateFailedException()
-        content = {"code": "2000", "message": "SUCCESS", "data": {"multipleStressIndex": stress_index_arr}}
-        return JSONResponse(content=content, status_code=200)
+        response = StressData(stress=stress_idx)
+        return JSONResponse(content=SuccessResponse(data=response.model_dump()).model_dump(), status_code=200)
     except CalculateFailedException as e:
-        error_content = {"code": "5000", "message": str(e), "data": None}
-        return JSONResponse(content=error_content, status_code=500)
+        return JSONResponse(content=ErrorResponse(code="5000", message=str(e)), status_code=500)
+
+
+@router.post("/fast/stress/overview")
+def calc_multiple_stress_index(data: StressOverviewRequest):
+    try:
+        stress_idx_arr = get_multiple_stress_index(data.hr_data)
+        if stress_idx_arr is None:
+            raise CalculateFailedException()
+        avg_arr, start_idx, end_idx = find_healing_course_idx(stress_idx_arr) if data.walking_time >= 300 else None
+        response = StressOverview(
+            max_stress=max(avg_arr),
+            min_stress=min(avg_arr),
+            healing_stress_avg=min(avg_arr),
+            start_idx=start_idx,
+            end_idx=end_idx,
+            stress_indices=stress_idx_arr
+        )
+        return JSONResponse(content=SuccessResponse(data=response.model_dump()).model_dump(), status_code=200)
+    except CalculateFailedException as e:
+        return JSONResponse(content=ErrorResponse(code="5000", message=str(e)), status_code=500)
