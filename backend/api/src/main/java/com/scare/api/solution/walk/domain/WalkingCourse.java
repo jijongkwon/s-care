e5330c6a28621da8eb0f244a99fc932ac3925de5
jijongkwon <help3451@naver.com>
2024-11-01 15:14:36 +0900

    feat: WalkingCourse Member 객체 추가

--- backend/api/src/main/java/com/scare/api/solution/walk/domain/WalkingCourse.java
+++ backend/api/src/main/java/com/scare/api/solution/walk/domain/WalkingCourse.java
@@ -2,11 +2,16 @@ package com.scare.api.solution.walk.domain;
 
 import java.time.LocalDateTime;
 
+import com.scare.api.member.domain.Member;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -48,9 +53,13 @@ public class WalkingCourse {
 	@Column(name = "finished_at", nullable = false)
 	private LocalDateTime finishedAt;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	Member member;
+
 	@Builder
 	public WalkingCourse(double distance, double minStress, double maxStress, double healingStressAvg, Integer startIdx,
-		Integer endIdx, LocalDateTime startedAt, LocalDateTime finishedAt) {
+		Integer endIdx, LocalDateTime startedAt, LocalDateTime finishedAt, Member member) {
 		this.distance = distance;
 		this.minStress = minStress;
 		this.maxStress = maxStress;
@@ -59,5 +68,6 @@ public class WalkingCourse {
 		this.endIdx = endIdx;
 		this.startedAt = startedAt;
 		this.finishedAt = finishedAt;
+		this.member = member;
 	}
 }
