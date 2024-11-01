987ff56950fd755b7a38008feacffc038d65e333
김연욱 <ccoma511@naver.com>
2024-11-01 09:53:10 +0900

    feat: Object Mapper 추가

--- backend/api/src/main/java/com/scare/api/core/config/security/SecurityConfig.java
+++ backend/api/src/main/java/com/scare/api/core/config/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scare.api.core.jwt.filter.JWTFilter;
 import com.scare.api.core.jwt.util.JWTUtil;
 
@@ -20,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
 	private final JWTUtil jwtUtil;
+	private final ObjectMapper objectMapper;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,10 +32,10 @@ public class SecurityConfig {
 		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 		http.authorizeHttpRequests(auth -> auth
-			.requestMatchers("/api/v*/members/login").permitAll()
+			.requestMatchers("/api/v*/members/auth/login").permitAll()
 			.anyRequest().authenticated());
 
-		http.addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+		http.addFilterBefore(new JWTFilter(jwtUtil, objectMapper), UsernamePasswordAuthenticationFilter.class);
 
 		// http.formLogin(AbstractHttpConfigurer::disable); // formLogin 사용 비활성화
 
