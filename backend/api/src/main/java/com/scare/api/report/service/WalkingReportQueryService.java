11cbc0f0a3f77afff9b3881dbf58b2fc651b7b3e
simhani1 <simhani1@gmail.com>
2024-11-04 12:36:08 +0900

    feat: 산책 레포트 service 추가

---
+++ backend/api/src/main/java/com/scare/api/report/service/WalkingReportQueryService.java
@@ -0,0 +1,11 @@
+package com.scare.api.report.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class WalkingReportQueryService {
+
+}
