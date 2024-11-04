c2241d60e8d0df08e8ffbbd25d1710517abe7e65
jijongkwon <help3451@naver.com>
2024-11-04 15:13:50 +0900

    feat: WalkingException handler 처리
    - 저장 예외처리
    - 그외 예외처리

---
+++ backend/api/src/main/java/com/scare/api/solution/walk/exception/WalkingCourseDataSaveException.java
@@ -0,0 +1,8 @@
+package com.scare.api.solution.walk.exception;
+
+public class WalkingCourseDataSaveException extends WalkingCourseException {
+
+	public WalkingCourseDataSaveException() {
+		super();
+	}
+}
