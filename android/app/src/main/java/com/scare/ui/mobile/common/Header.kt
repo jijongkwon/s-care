d434b141f704daa08cf168d0ad4d1576107e46ea
Doeun <ide05484@gmail.com>
2024-11-04 14:59:19 +0900

    feat: add Header vertical, horizontal padding

--- android/app/src/main/java/com/scare/ui/mobile/common/Header.kt
+++ android/app/src/main/java/com/scare/ui/mobile/common/Header.kt
@@ -14,10 +14,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.scare.R
 
+@Preview
 @Composable
 fun TheHeader(
     imageUrl: String? = null,
@@ -27,7 +29,7 @@ fun TheHeader(
     val navController = LocalNavController.current
 
     Row(
-        modifier = Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.background).statusBarsPadding(),
+        modifier = Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.background).statusBarsPadding().padding(horizontal = 21.dp, vertical = 14.dp),
         horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically
     ) {
         if (isMainPage && imageUrl != null) {
