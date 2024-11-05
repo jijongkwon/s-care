b5303491019272b042b54ccdcb0615a0763fa92e
김연욱 <ccoma511@naver.com>
2024-11-05 12:35:31 +0900

    feat: Repository 어노테이션 제거

--- backend/api/src/main/java/com/scare/api/member/repository/MemberRepository.java
+++ backend/api/src/main/java/com/scare/api/member/repository/MemberRepository.java
@@ -1,11 +1,9 @@
 package com.scare.api.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.scare.api.member.domain.Member;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
