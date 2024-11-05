ded68cae816663cd347e328759ba5400ded64ef0
김연욱 <ccoma511@naver.com>
2024-11-05 13:12:24 +0900

    feat: Slf4j 어노테이션 제거

--- backend/api/src/main/java/com/scare/api/member/controller/AuthController.java
+++ backend/api/src/main/java/com/scare/api/member/controller/AuthController.java
@@ -19,9 +19,7 @@ import com.scare.api.member.service.dto.LoginDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/v1/members/auth")
 @RequiredArgsConstructor
