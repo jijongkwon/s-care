2196cf532ab1f2cec3bf16b2d5dbb5e083739681
LEEJW1953 <jiwonlee1953@naver.com>
2024-11-08 20:00:41 +0900

    feat: 산책 API 연동

--- android/app/src/main/java/com/scare/ui/mobile/course/component/CourseItem.kt
+++ android/app/src/main/java/com/scare/ui/mobile/course/component/CourseItem.kt
@@ -2,26 +2,22 @@ package com.scare.ui.mobile.course.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.scare.data.course.dto.CourseResponseDTO
-import com.scare.ui.theme.DarkColorScheme
-import com.scare.ui.theme.LightColorScheme
-import com.scare.ui.theme.ScareTheme
 import com.scare.ui.theme.Typography
 import com.scare.util.calculateTimeDifference
 import com.scare.util.formatDateTime
@@ -29,19 +25,13 @@ import com.scare.util.formatDateTime
 @ExperimentalNaverMapApi
 @Composable
 fun CourseItem(course: CourseResponseDTO) {
-    val darkTheme = isSystemInDarkTheme()
-    val colorScheme = if (darkTheme) {
-        DarkColorScheme
-    } else {
-        LightColorScheme
-    }
 
     var isExpanded by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .background(colorScheme.background),
+            .background(MaterialTheme.colorScheme.background),
     ) {
         Column {
             Row(
@@ -57,7 +47,7 @@ fun CourseItem(course: CourseResponseDTO) {
                 ) {
                     Text(
                         text = formatDateTime(course.startedAt),
-                        style = Typography.bodyLarge.copy(color = colorScheme.onSurface),
+                        style = Typography.bodyLarge.copy(color = MaterialTheme.colorScheme.onSurface),
                     )
 
                     Spacer(modifier = Modifier.height(8.dp))
@@ -72,57 +62,39 @@ fun CourseItem(course: CourseResponseDTO) {
                                 course.finishedAt
                             ),
                             style = Typography.titleLarge.copy(
-                                color = colorScheme.tertiary, fontWeight = FontWeight.Bold, fontSize = 24.sp
+                                color = MaterialTheme.colorScheme.tertiary,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 24.sp
                             )
                         )
                         Spacer(modifier = Modifier.width(4.dp))
                         Text(
-                            text = "총 시간", style = Typography.labelMedium.copy(color = colorScheme.onSurface)
+                            text = "총 시간",
+                            style = Typography.labelMedium.copy(color = MaterialTheme.colorScheme.onSurface)
                         )
                     }
                 }
 
                 Spacer(modifier = Modifier.height(8.dp))
 
-                // 아이콘
                 Icon(
                     modifier = Modifier.clickable(onClick = { isExpanded = !isExpanded }),
                     imageVector = if (isExpanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
                     contentDescription = "Detail",
-                    tint = colorScheme.onSurface,
+                    tint = MaterialTheme.colorScheme.onSurface,
                 )
             }
 
             HorizontalDivider(
-                color = colorScheme.tertiary,
+                color = MaterialTheme.colorScheme.tertiary,
                 thickness = 0.5.dp,
                 modifier = Modifier
                     .fillMaxWidth()
             )
 
             if (isExpanded) {
-                CourseDetail(course)
+                CourseDetailMap(course)
             }
         }
-    }
-}
-
-@ExperimentalNaverMapApi
-@Preview(showBackground = true, uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES)
-@Composable
-fun CourseItemPreview() {
-    val course = CourseResponseDTO(
-        courseId = 46,
-        startedAt = "2024-11-01T16:42:23",
-        finishedAt = "2024-11-01T17:23:44",
-        posList = null,
-        startIdx = 0,
-        endIdx = 0,
-        maxStress = 10.0,
-        minStress = 20.0,
-        distance = 10.0
-    )
-    ScareTheme {
-        CourseItem(course)
     }
 }
--- android/app/src/main/java/com/scare/ui/mobile/course/component/CourseList.kt
+++ android/app/src/main/java/com/scare/ui/mobile/course/component/CourseList.kt
@@ -7,10 +7,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.scare.data.course.dto.CourseResponseDTO
-import com.scare.ui.theme.ScareTheme
 
 @ExperimentalNaverMapApi
 @Composable
@@ -23,38 +21,5 @@ fun CourseList(courseList: List<CourseResponseDTO>) {
         items(courseList) { course ->
             CourseItem(course)
         }
-    }
-}
-
-@ExperimentalNaverMapApi
-@Preview(showBackground = true, uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES)
-@Composable
-fun CourseListPreview() {
-    val courseList: List<CourseResponseDTO> = listOf(
-        CourseResponseDTO(
-            courseId = 46,
-            startedAt = "2024-11-01T16:42:23",
-            finishedAt = "2024-11-01T16:42:23",
-            posList = null,
-            startIdx = 0,
-            endIdx = 0,
-            maxStress = 10.0,
-            minStress = 20.0,
-            distance = 10.0
-        ),
-        CourseResponseDTO(
-            courseId = 45,
-            startedAt = "2024-11-01T16:42:22",
-            finishedAt = "2024-11-01T16:42:22",
-            posList = null,
-            startIdx = 0,
-            endIdx = 0,
-            maxStress = 10.0,
-            minStress = 20.0,
-            distance = 10.0
-        )
-    )
-    ScareTheme {
-        CourseList(courseList)
     }
 }
