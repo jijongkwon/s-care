bfd382600019c104ebcda7bfba3a3e054fb4ffcd
LEEJW1953 <jiwonlee1953@naver.com>
2024-11-08 20:00:10 +0900

    feat: LocalCourseViewModel 구현

--- android/app/src/main/java/com/scare/ui/mobile/common/CompositionLocals.kt
+++ android/app/src/main/java/com/scare/ui/mobile/common/CompositionLocals.kt
@@ -2,5 +2,8 @@ package com.scare.ui.mobile.common
 
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.navigation.NavHostController
+import com.scare.ui.mobile.viewmodel.course.CourseViewModel
 
 val LocalNavController = staticCompositionLocalOf<NavHostController?> { null }
+
+val LocalCourseViewModel = staticCompositionLocalOf<CourseViewModel?> { null }
