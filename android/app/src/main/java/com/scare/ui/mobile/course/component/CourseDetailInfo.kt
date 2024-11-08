9087221aae7b19de00aab223c6fd1c24c51313ab
LEEJW1953 <jiwonlee1953@naver.com>
2024-11-08 20:01:13 +0900

    feat: 내 산책 상세보기 구현

---
+++ android/app/src/main/java/com/scare/ui/mobile/course/component/CourseDetailInfo.kt
@@ -0,0 +1,82 @@
+package com.scare.ui.mobile.course.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.scare.R
+import com.scare.data.course.dto.CourseResponseDTO
+import com.scare.ui.theme.Typography
+import com.scare.util.calculateTimeDifference
+
+@Composable
+fun CourseDetailInfo(
+    modifier: Modifier,
+    course: CourseResponseDTO
+) {
+    Box(modifier = modifier) {
+        Row(
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(top = 20.dp, end = 20.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.tertiary)
+                .padding(horizontal = 24.dp, vertical = 8.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "총 시간",
+                    color = MaterialTheme.colorScheme.background,
+                )
+                Text(
+                    text = calculateTimeDifference(
+                        course.startedAt,
+                        course.finishedAt
+                    ),
+                    color = MaterialTheme.colorScheme.background,
+                    style = Typography.titleLarge.copy(
+                        color = MaterialTheme.colorScheme.tertiary, fontWeight = FontWeight.Bold, fontSize = 24.sp
+                    )
+                )
+                Text(
+                    text = "내 스트레스",
+                    color = MaterialTheme.colorScheme.background
+                )
+                Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = (course.maxStress - course.minStress).toString(),
+                        color = MaterialTheme.colorScheme.background,
+                        style = Typography.titleLarge.copy(
+                            color = MaterialTheme.colorScheme.tertiary,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 24.sp
+                        )
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.arrow_down),
+                        contentDescription = "Stress Down",
+                        tint = Color.Unspecified
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ android/app/src/main/java/com/scare/ui/mobile/course/component/CourseDetailMap.kt
@@ -0,0 +1,98 @@
+package com.scare.ui.mobile.course.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.LatLngBounds
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.compose.*
+import com.naver.maps.map.overlay.OverlayImage
+import com.scare.R
+import com.scare.data.course.dto.CourseResponseDTO
+import com.scare.ui.mobile.common.LocalCourseViewModel
+
+@ExperimentalNaverMapApi
+@Composable
+fun CourseDetailMap(course: CourseResponseDTO) {
+
+    val localCourseViewModel = LocalCourseViewModel.current
+
+    val courseDetail by localCourseViewModel!!.courseDetail.collectAsState()
+
+    LaunchedEffect(Unit) {
+        localCourseViewModel!!.fetchWalkingCourseDetail(course.courseId)
+    }
+
+    var mapProperties by remember {
+        mutableStateOf(
+            MapProperties(lightness = -0.2f)
+        )
+    }
+    var mapUiSettings by remember {
+        mutableStateOf(
+            MapUiSettings(isScrollGesturesEnabled = false, isZoomControlEnabled = false)
+        )
+    }
+
+    val coords = if (courseDetail != null) {
+        courseDetail!!.posList!!.map { position -> LatLng(position.lat, position.lng) }
+    } else {
+        listOf()
+    }
+
+    val cameraPositionState = rememberCameraPositionState()
+
+    Box(Modifier.fillMaxWidth().height(600.dp)) {
+        NaverMap(
+            properties = mapProperties,
+            uiSettings = mapUiSettings,
+            cameraPositionState = cameraPositionState,
+            onMapLoaded = {
+                if (coords.isNotEmpty()) {
+                    val bounds = LatLngBounds.Builder().apply {
+                        coords.forEach { include(it) }
+                    }.build()
+
+                    val cameraUpdate = CameraUpdate.fitBounds(bounds, 100, 600, 100, 100)
+                    cameraPositionState.move(cameraUpdate)
+                }
+            }
+        ) {
+            if (coords.isNotEmpty()) {
+                val firstCoord = coords.first()
+                val lastCoord = coords.last()
+                Marker(
+                    state = MarkerState(
+                        position = LatLng(
+                            firstCoord.latitude, firstCoord.longitude
+                        )
+                    ),
+                    icon = OverlayImage.fromResource(R.drawable.first_marker)
+                )
+                Marker(
+                    state = MarkerState(
+                        position = LatLng(
+                            lastCoord.latitude, lastCoord.longitude
+                        )
+                    ),
+                    icon = OverlayImage.fromResource(R.drawable.last_marker)
+                )
+                PathOverlay(
+                    coords = coords,
+                    color = MaterialTheme.colorScheme.tertiary,
+                    outlineColor = MaterialTheme.colorScheme.tertiary,
+                    width = 5.dp,
+                )
+            }
+        }
+        CourseDetailInfo(
+            modifier = Modifier.fillMaxSize(), course
+        )
+    }
+}
