a3b4b97d20ce9e2739a6c8ecac15bc7cf98b547c
김예훈 <rladpgns00@gmail.com>
2024-11-11 10:56:11 +0900

    feat: wear os 산책 시작, 종료 데이터 모바일로 동기화

--- android/wear/src/main/java/com/scare/presentation/walk/WalkViewModel.kt
+++ android/wear/src/main/java/com/scare/presentation/walk/WalkViewModel.kt
@@ -1,9 +1,13 @@
 package com.scare.presentation.walk
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.google.android.gms.wearable.PutDataMapRequest
+import com.google.android.gms.wearable.Wearable
+import com.scare.TAG
 import com.scare.datastore.IS_WALK_KEY
 import com.scare.datastore.saveWalkStatus
 import com.scare.datastore.walkStore
@@ -16,6 +20,8 @@ class WalkViewModel(context: Context) : ViewModel() {
 
     private val _isWalk = MutableStateFlow(false)
     val isWalk: Flow<Boolean> = _isWalk
+
+    private val dataClient = Wearable.getDataClient(context)
 
     init {
         // DataStore에서 산책 상태를 관찰
@@ -32,6 +38,22 @@ class WalkViewModel(context: Context) : ViewModel() {
     fun updateWalkStatus(context: Context, isWalk: Boolean) {
         saveWalkStatus(context, isWalk)
         _isWalk.value = isWalk
+
+        sendToHandheldDevice(isWalk)
+    }
+
+    private fun sendToHandheldDevice(isWalk: Boolean) {
+        val putDataReq = PutDataMapRequest.create("/walk").apply {
+            dataMap.putBoolean("isWalk", isWalk)
+            dataMap.putLong("timestamp", System.currentTimeMillis())  // Optional: add a timestamp
+        }.asPutDataRequest()
+            .setUrgent()
+
+        dataClient.putDataItem(putDataReq).addOnSuccessListener {
+            Log.d(TAG, "Walk state is sent to handheld device successfully")
+        }.addOnFailureListener { e ->
+            Log.e(TAG, "Failed to send walk state to handheld device", e)
+        }
     }
 }
 
