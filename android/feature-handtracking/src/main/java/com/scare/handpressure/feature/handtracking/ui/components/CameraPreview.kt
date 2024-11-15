2f5eb62f1fab6978c235f9314c57e62c4d0deeee
jijongkwon <help3451@naver.com>
2024-11-15 16:28:55 +0900

    feat: 상태 추적 및 중복 방지 설정

--- android/feature-handtracking/src/main/java/com/scare/handpressure/feature/handtracking/ui/components/CameraPreview.kt
+++ android/feature-handtracking/src/main/java/com/scare/handpressure/feature/handtracking/ui/components/CameraPreview.kt
@@ -1,5 +1,7 @@
 package com.scare.handpressure.feature.handtracking.ui.components
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.camera.core.AspectRatio
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
@@ -10,6 +12,7 @@ import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -27,6 +30,9 @@ fun CameraPreview(
     val lifecycleOwner = LocalLifecycleOwner.current
     val cameraExecutor = remember { Executors.newSingleThreadExecutor() }
 
+    // 카메라 설정이 완료되었는지 추적
+    val isConfigured = remember { mutableStateOf(false) }
+
     DisposableEffect(Unit) {
         onDispose {
             cameraExecutor.shutdown()
@@ -42,39 +48,50 @@ fun CameraPreview(
             }
         },
         update = { previewView ->
+            // 이미 설정되었다면 스킵
+            if (isConfigured.value) {
+                return@AndroidView
+            }
+
             val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
 
             cameraProviderFuture.addListener({
-                val cameraProvider = cameraProviderFuture.get()
+                if (!isConfigured.value) {
+                    val cameraProvider = cameraProviderFuture.get()
 
-                val preview = Preview.Builder()
-                    .setTargetAspectRatio(AspectRatio.RATIO_4_3)
-                    .build()
-                    .also {
-                        it.setSurfaceProvider(previewView.surfaceProvider)
-                    }
-
-                val imageAnalyzer = ImageAnalysis.Builder()
-                    .setTargetAspectRatio(AspectRatio.RATIO_4_3)
-                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                    .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
-                    .build()
-                    .also {
-                        it.setAnalyzer(cameraExecutor) { imageProxy ->
-                            onFrameAvailable(imageProxy)
+                    val preview = Preview.Builder()
+                        .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                        .build()
+                        .apply {
+                            setSurfaceProvider(
+                                ContextCompat.getMainExecutor(context),
+                                previewView.surfaceProvider
+                            )
                         }
-                    }
 
-                try {
-                    cameraProvider.unbindAll()
-                    cameraProvider.bindToLifecycle(
-                        lifecycleOwner,
-                        CameraSelector.DEFAULT_FRONT_CAMERA,
-                        preview,
-                        imageAnalyzer
-                    )
-                } catch (e: Exception) {
-                    e.printStackTrace()
+                    val imageAnalyzer = ImageAnalysis.Builder()
+                        .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                        .build()
+                        .also {
+                            it.setAnalyzer(cameraExecutor) { imageProxy ->
+                                onFrameAvailable(imageProxy)
+                            }
+                        }
+
+                    try {
+                        cameraProvider.unbindAll()
+                        val camera = cameraProvider.bindToLifecycle(
+                            lifecycleOwner,
+                            CameraSelector.DEFAULT_FRONT_CAMERA,
+                            preview,
+                            imageAnalyzer
+                        )
+                        isConfigured.value = true
+                    } catch (e: Exception) {
+                        Log.e(TAG, "jjk camera: 바인딩 실패", e)
+                    }
                 }
             }, ContextCompat.getMainExecutor(context))
         }
