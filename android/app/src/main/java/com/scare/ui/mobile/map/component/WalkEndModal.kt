cc65cbd17626d01f5be203ba7221a3ed0ee7adf9
LEEJW1953 <jiwonlee1953@naver.com>
2024-11-17 23:19:08 +0900

    feat: 산책 종료 시 로딩 처리

--- android/app/src/main/java/com/scare/ui/mobile/map/component/WalkEndModal.kt
+++ android/app/src/main/java/com/scare/ui/mobile/map/component/WalkEndModal.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.scare.R
+import com.scare.ui.mobile.calender.component.LoadingAnimation
 import com.scare.ui.mobile.common.LocalNavController
 import com.scare.ui.theme.Gray
 
@@ -20,7 +21,8 @@ fun WalkEndModal(
     modifier: Modifier = Modifier,
     onClose: () -> Unit,
     handleWalkStop: () -> Unit,
-    isWalkComplete: Boolean
+    isWalkComplete: Boolean,
+    isLoading: Boolean
 ) {
 
     val navController = LocalNavController.current
@@ -40,103 +42,113 @@ fun WalkEndModal(
             ),
             elevation = CardDefaults.cardElevation(8.dp),
         ) {
-            Column(
-                modifier = Modifier.padding(20.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
+            if (isLoading) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.Center
                 ) {
-                    if (isWalkComplete) {
-                        Image(
-                            painter = painterResource(R.drawable.happy_dog_face),
-                            contentDescription = "happy_dog_face",
-                            modifier = Modifier
-                                .width(60.dp)
-                                .height(60.dp)
-                        )
-                    } else {
-                        Image(
-                            painter = painterResource(R.drawable.gloomy_dog_face),
-                            contentDescription = "gloomy_dog_face",
-                            modifier = Modifier
-                                .width(60.dp)
-                                .height(60.dp)
-                        )
-                    }
-                    if (isWalkComplete) {
-                        Text(
-                            text = "산책을 종료했습니다!",
-                            fontSize = 20.sp,
-                            textAlign = TextAlign.Center,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                    } else {
-                        Text(
-                            text = "5분 이상의 산책만 기록됩니다.\n산책을 종료할까요?",
-                            fontSize = 16.sp,
-                            textAlign = TextAlign.Center,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
+                    LoadingAnimation(modifier = Modifier.size(200.dp))
                 }
-                Spacer(modifier = Modifier.height(16.dp))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
+            } else {
+                Column(
+                    modifier = Modifier.padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceBetween
                 ) {
-                    if (isWalkComplete) {
-                        Button(
-                            onClick = { onClose() },
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = MaterialTheme.colorScheme.background,
-                                contentColor = MaterialTheme.colorScheme.onSurface
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        if (isWalkComplete) {
+                            Image(
+                                painter = painterResource(R.drawable.happy_dog_face),
+                                contentDescription = "happy_dog_face",
+                                modifier = Modifier
+                                    .width(60.dp)
+                                    .height(60.dp)
                             )
-                        ) {
-                            Text(
-                                text = "닫기",
+                        } else {
+                            Image(
+                                painter = painterResource(R.drawable.gloomy_dog_face),
+                                contentDescription = "gloomy_dog_face",
+                                modifier = Modifier
+                                    .width(60.dp)
+                                    .height(60.dp)
                             )
                         }
-                    } else {
-                        Button(
-                            onClick = { handleWalkStop() },
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = MaterialTheme.colorScheme.background,
-                                contentColor = MaterialTheme.colorScheme.onSurface
-                            )
-                        ) {
+                        if (isWalkComplete) {
                             Text(
-                                text = "산책 그만하기",
+                                text = "산책을 종료했습니다!",
+                                fontSize = 20.sp,
+                                textAlign = TextAlign.Center,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
+                        } else {
+                            Text(
+                                text = "5분 이상의 산책만 기록됩니다.\n산책을 종료할까요?",
+                                fontSize = 16.sp,
+                                textAlign = TextAlign.Center,
+                                color = MaterialTheme.colorScheme.onSurface
                             )
                         }
                     }
-                    Spacer(modifier = Modifier.width(8.dp))
-                    if (isWalkComplete) {
-                        Button(
-                            onClick = { navController?.navigate("walk") },
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = MaterialTheme.colorScheme.tertiary,
-                                contentColor = MaterialTheme.colorScheme.background
-                            )
-                        ) {
-                            Text(
-                                text = "기록 보기",
-                            )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        if (isWalkComplete) {
+                            Button(
+                                onClick = { onClose() },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = MaterialTheme.colorScheme.background,
+                                    contentColor = MaterialTheme.colorScheme.onSurface
+                                )
+                            ) {
+                                Text(
+                                    text = "닫기",
+                                )
+                            }
+                        } else {
+                            Button(
+                                onClick = { handleWalkStop() },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = MaterialTheme.colorScheme.background,
+                                    contentColor = MaterialTheme.colorScheme.onSurface
+                                )
+                            ) {
+                                Text(
+                                    text = "산책 그만하기",
+                                )
+                            }
                         }
-                    } else {
-                        Button(
-                            onClick = { onClose() },
-                            colors = ButtonDefaults.buttonColors(
-                                containerColor = MaterialTheme.colorScheme.tertiary,
-                                contentColor = MaterialTheme.colorScheme.background
-                            )
-                        ) {
-                            Text(
-                                text = "산책 계속하기",
-                            )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        if (isWalkComplete) {
+                            Button(
+                                onClick = { navController?.navigate("walk") },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = MaterialTheme.colorScheme.tertiary,
+                                    contentColor = MaterialTheme.colorScheme.background
+                                )
+                            ) {
+                                Text(
+                                    text = "기록 보기",
+                                )
+                            }
+                        } else {
+                            Button(
+                                onClick = { onClose() },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = MaterialTheme.colorScheme.tertiary,
+                                    contentColor = MaterialTheme.colorScheme.background
+                                )
+                            ) {
+                                Text(
+                                    text = "산책 계속하기",
+                                )
+                            }
                         }
                     }
                 }
