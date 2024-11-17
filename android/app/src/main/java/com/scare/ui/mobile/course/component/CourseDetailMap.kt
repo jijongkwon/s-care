f283263a2b8a8140a445026f53e853981d0af73d
LEEJW1953 <jiwonlee1953@naver.com>
2024-11-17 23:18:39 +0900

    feat: 산책 기록 로딩, 예외 처리

--- android/app/src/main/java/com/scare/ui/mobile/course/component/CourseDetailMap.kt
+++ android/app/src/main/java/com/scare/ui/mobile/course/component/CourseDetailMap.kt
@@ -1,11 +1,10 @@
 package com.scare.ui.mobile.course.component
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.naver.maps.geometry.LatLng
@@ -15,6 +14,7 @@ import com.naver.maps.map.compose.*
 import com.naver.maps.map.overlay.OverlayImage
 import com.scare.R
 import com.scare.data.course.dto.CourseResponseDTO
+import com.scare.ui.mobile.calender.component.LoadingAnimation
 import com.scare.ui.mobile.common.LocalCourseViewModel
 import com.scare.ui.theme.Gray
 
@@ -25,9 +25,11 @@ fun CourseDetailMap(course: CourseResponseDTO) {
     val localCourseViewModel = LocalCourseViewModel.current
 
     val courseDetail by localCourseViewModel!!.courseDetail.collectAsState()
+    var isDataLoaded by remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         localCourseViewModel!!.fetchWalkingCourseDetail(course.courseId)
+        isDataLoaded = true
     }
 
     var mapProperties by remember {
@@ -49,68 +51,91 @@ fun CourseDetailMap(course: CourseResponseDTO) {
 
     val cameraPositionState = rememberCameraPositionState()
 
-    Box(
-        Modifier
-            .fillMaxWidth()
-            .height(600.dp)
-    ) {
-        NaverMap(
-            properties = mapProperties,
-            uiSettings = mapUiSettings,
-            cameraPositionState = cameraPositionState,
-            onMapLoaded = {
-                if (coords.isNotEmpty()) {
-                    val bounds = LatLngBounds.Builder().apply {
-                        coords.forEach { include(it) }
-                    }.build()
+    if (isDataLoaded) {
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .height(600.dp)
+        ) {
+            NaverMap(
+                properties = mapProperties,
+                uiSettings = mapUiSettings,
+                cameraPositionState = cameraPositionState,
+                onMapLoaded = {
+                    if (coords.isNotEmpty()) {
+                        val bounds = LatLngBounds.Builder().apply {
+                            coords.forEach { include(it) }
+                        }.build()
 
-                    val cameraUpdate = CameraUpdate.fitBounds(bounds, 100, 600, 100, 100)
-                    cameraPositionState.move(cameraUpdate)
+                        val cameraUpdate = CameraUpdate.fitBounds(bounds, 100, 600, 100, 100)
+                        cameraPositionState.move(cameraUpdate)
+                    }
+                }
+            ) {
+                if (coords.isNotEmpty()) {
+                    val firstCoord = coords.first()
+                    val lastCoord = coords.last()
+                    Marker(
+                        state = MarkerState(
+                            position = LatLng(
+                                firstCoord.latitude, firstCoord.longitude
+                            )
+                        ),
+                        icon = OverlayImage.fromResource(R.drawable.first_marker)
+                    )
+                    Marker(
+                        state = MarkerState(
+                            position = LatLng(
+                                lastCoord.latitude, lastCoord.longitude
+                            )
+                        ),
+                        icon = OverlayImage.fromResource(R.drawable.last_marker)
+                    )
+                    if (coords.size >= 2) {
+                        ArrowheadPathOverlay(
+                            coords = coords,
+                            color = Gray,
+                            outlineColor = Gray,
+                            width = 5.dp,
+                            globalZIndex = 0
+                        )
+                    }
+                    if (course.startIdx < coords.size &&
+                        course.endIdx < coords.size &&
+                        course.startIdx <= course.endIdx
+                    ) {
+                        val bestCourse = coords.slice(course.startIdx..course.endIdx)
+                        if (bestCourse.size >= 2) {
+                            PathOverlay(
+                                coords = bestCourse,
+                                color = MaterialTheme.colorScheme.tertiary,
+                                outlineColor = MaterialTheme.colorScheme.tertiary,
+                                width = 5.dp,
+                                globalZIndex = 1
+                            )
+                        }
+                    }
                 }
             }
+            CourseDetailInfo(
+                modifier = Modifier.fillMaxSize(), course
+            )
+        }
+    } else {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
-            if (coords.isNotEmpty()) {
-                val firstCoord = coords.first()
-                val lastCoord = coords.last()
-                Marker(
-                    state = MarkerState(
-                        position = LatLng(
-                            firstCoord.latitude, firstCoord.longitude
-                        )
-                    ),
-                    icon = OverlayImage.fromResource(R.drawable.first_marker)
-                )
-                Marker(
-                    state = MarkerState(
-                        position = LatLng(
-                            lastCoord.latitude, lastCoord.longitude
-                        )
-                    ),
-                    icon = OverlayImage.fromResource(R.drawable.last_marker)
-                )
-                if (coords.size >= 2) {
-                    ArrowheadPathOverlay(
-                        coords = coords,
-                        color = Gray,
-                        outlineColor = Gray,
-                        width = 5.dp,
-                        globalZIndex = 0
-                    )
-                }
-                val bestCourse = coords.slice(course.startIdx..course.endIdx)
-                if (bestCourse.size >= 2) {
-                    PathOverlay(
-                        coords = bestCourse,
-                        color = MaterialTheme.colorScheme.tertiary,
-                        outlineColor = MaterialTheme.colorScheme.tertiary,
-                        width = 5.dp,
-                        globalZIndex = 1
-                    )
-                }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                LoadingAnimation(modifier = Modifier.size(500.dp))
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text("로딩 중...", style = MaterialTheme.typography.titleLarge)
             }
         }
-        CourseDetailInfo(
-            modifier = Modifier.fillMaxSize(), course
-        )
     }
 }
