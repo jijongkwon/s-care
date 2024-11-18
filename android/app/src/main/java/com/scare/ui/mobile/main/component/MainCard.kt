7392002209a50a89d6b5352816b7a9a1323f996f
LEEJW1953 <jiwonlee1953@naver.com>
2024-11-18 20:34:29 +0900

    design: 메인화면 솔루션 버튼 수정

--- android/app/src/main/java/com/scare/ui/mobile/main/component/MainCard.kt
+++ android/app/src/main/java/com/scare/ui/mobile/main/component/MainCard.kt
@@ -1,6 +1,7 @@
 package com.scare.ui.mobile.main.component
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
@@ -12,9 +13,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.scare.R
 import com.scare.ui.mobile.common.LocalNavController
 import com.scare.ui.mobile.viewmodel.pressure.Solution
 import com.scare.ui.theme.Brown
@@ -52,11 +55,16 @@ fun SolutionCard(solution: Solution) {
             navController?.navigate(solution.path)
         },
     ) {
-        Box(
+        Column(
             modifier = Modifier
                 .fillMaxWidth(),
-            contentAlignment = Alignment.Center
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            val buttonImage = if (isSystemInDarkTheme()) {
+                R.drawable.button_dark
+            } else {
+                R.drawable.button_light
+            }
             Text(
                 text = solution.title,
                 modifier = Modifier
@@ -65,6 +73,12 @@ fun SolutionCard(solution: Solution) {
                 style = MaterialTheme.typography.titleLarge.copy(
                     fontSize = 20.sp
                 )
+            )
+            Spacer(modifier = Modifier.height(18.dp))
+            Image(
+                painter = painterResource(buttonImage),
+                contentDescription = null,
+                modifier = Modifier.size(80.dp) // 아이콘 크기 설정
             )
         }
     }
